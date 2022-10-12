f51f50359c097a25dde3e322ca893d3ac99f979f
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-10-12 23:56:50 +0300

    Added Multi Line Task++: Hello World

---
+++ 2kyu/multi-line-task-hello-world-easier .js
@@ -0,0 +1,45 @@
+// You need to write a function f that returns the string Hello, world!.
+
+// Requirement: Every line must have at most 2 characters, and total number of lines must be less than 40.
+
+// Hint: It's possible to complete this in 28 lines only.
+
+c=
+`\
+c\
+o\
+n\
+s\
+t\
+r\
+u\
+c\
+t\
+o\
+r\
+`
+c[
+c]
+[c
+](
+`f
+=(
+)\
+=>
+'\
+H\
+e\
+l\
+l\
+o\
+,\
+ \
+w\
+o\
+r\
+l\
+d\
+!\
+'`
+)(
+)
