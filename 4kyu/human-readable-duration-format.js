ccbd3eb1b8458dab2b85dcc20f8b6e8acf07b14e
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-10-15 20:25:34 +0300

    Added Human Readable Duration Format

---
+++ 4kyu/human-readable-duration-format.js
@@ -0,0 +1,37 @@
+// Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+
+// The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
+// It is much easier to understand with an example:
+
+// * For seconds = 62, your function should return
+//     "1 minute and 2 seconds"
+// * For seconds = 3662, your function should return
+//     "1 hour, 1 minute and 2 seconds"
+// For the purpose of this Kata, a year is 365 days and a day is 24 hours.
+
+// My Solution
+const formatDuration = (seconds) => {
+  const time = {
+    year: 31536000,
+    day: 86400,
+    hour: 3600,
+    minute: 60,
+    second: 1,
+  };
+  let res = [];
+
+  if (seconds === 0) return "now";
+
+  for (let key in time) {
+    if (seconds >= time[key]) {
+      var val = Math.floor(seconds / time[key]);
+      res.push((val += val > 1 ? " " + key + "s" : " " + key));
+      seconds = seconds % time[key];
+    }
+  }
+
+  return res.length > 1
+    ? res.join(", ").replace(/,([^,]*)$/, " and" + "$1")
+    : res[0];
+};
