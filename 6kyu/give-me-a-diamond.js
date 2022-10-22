61e645f04a1f16c35ae3b400133173a98db1fcfb
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-10-23 00:55:39 +0300

    Added Give Me A Diamond

---
+++ 6kyu/give-me-a-diamond.js
@@ -0,0 +1,30 @@
+// Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+
+// Task
+// You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+// Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+// Examples
+// A size 3 diamond:
+
+//  *
+// ***
+//  *
+// ...which would appear as a string of " *\n***\n *\n"
+
+// My Solution
+const diamond = (n) => {
+  if (n <= 0 || n % 2 == 0) {
+    return null;
+  }
+
+  var middle = parseInt((n + 1) / 2);
+  var j,
+    diam = "";
+  for (var i = 1; i <= n; i++) {
+    j = i <= middle ? i : n - i + 1;
+    diam += " ".repeat(middle - j) + "*".repeat(2 * j - 1) + "\n";
+  }
+  return diam;
+};
