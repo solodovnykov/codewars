e94332cbd0b80eda101cfb42f2ecd936df71a626
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-10-31 23:29:34 +0200

    Added Squares Sequence

---
+++ 7kyu/squares-sequence.js
@@ -0,0 +1,19 @@
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+
+// Examples
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+
+const squares = (x, n) => {
+  let result = [];
+  let temp = x;
+
+  if (n <= 0) return [];
+
+  for (let i = 0; i < n; i++) {
+    result.push(temp);
+    temp = temp ** 2;
+  }
+
+  return result;
+};
